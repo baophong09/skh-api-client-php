0b217dc8d05cb4cdcb39f3c2facf30f55cb9bdea
Dinh Phong <dinhphong.developer@gmail.com>
2016-09-17 03:07:24 +0700

    crypt & client

--- src/Skh/Client.php
+++ src/Skh/Client.php
@@ -17,6 +17,8 @@ class Client
 
     private $request;
 
+    private $cookie;
+
     const API_SERVER = 'http://api.sukienhay.com/';
 
     const VERSION = 'v1/';   
@@ -28,9 +30,10 @@ class Client
         $this->serverName = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '';
         $this->request = new Request();
         $this->token = new Token($this->secretKey);
+        $this->cookie = isset($_COOKIE["SKH_API_COOKIE"]) ? $_COOKIE["SKH_API_COOKIE"] : "";
     }
 
-    private function getVerifyApplication($publicKey, $secretKey)
+    private function getVerifyApplication($publicKey, $secretKey, $cookie)
     {
         $data = [
             'iat'           =>  time(),
@@ -39,7 +42,8 @@ class Client
             'exp'           =>  time() + 60,
             'data'          =>  [
                 'public_key'    =>  $publicKey,
-                'secret_key'    =>  $this->secretKey
+                'secret_key'    =>  $secretKey,
+                'cookie'        =>  $cookie
             ]
         ];
 
@@ -49,11 +53,19 @@ class Client
     public function getAccessToken()
     {
         $res = $this->post('token/generate', [
-            'verify_token'  =>  $this->getVerifyApplication($this->publicKey, $this->secretKey),
+            'verify_token'  =>  $this->getVerifyApplication($this->publicKey, $this->secretKey, $this->cookie),
             'public_key'    =>  $this->publicKey
         ]);
 
         dd($res);
+
+        $decoded = json_decode($res);
+
+        if($decoded && $decoded->success === true && $decoded->access_token) {
+            setcookie("SKH_API_COOKIE", $this->token->get($decoded->access_token), $decoded->ei);
+        }
+
+        return $res;
     }
 
     public function get($url, $params)
