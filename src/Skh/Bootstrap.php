0e186323bcf5dbd1d9844bed5a41284cbae52952
Dinh Phong <dinhphong.developer@gmail.com>
2016-10-11 00:55:51 +0700

    Bootstrap fix

--- src/Skh/Bootstrap.php
+++ src/Skh/Bootstrap.php
@@ -1,3 +1,19 @@
+<?php
+
+/*
+ * +------------------------------------------------------------------------+
+ * | SUKIENHAY.com API - API CLIENT                                         |
+ * | @package       : skh-client-api                                        |
+ * | @authors       : Dinh Phong                                            |
+ * | @github        : baophong09                                            |
+ * | @copyright     : Copyright (c) 2016, SUKIENHAY.COM                     |
+ * | @since         : Version 1.0.0                                         |
+ * | @website       : https://sukienhay.com                                 |
+ * | @e-mail        : dinhphong.developer@gmail.com                         |
+ * | @require       : PHP version >= 5.5.0                                  |
+ * +------------------------------------------------------------------------+
+ */
+
 /**
  * Bootstrap file, startup when autoload 
  */
